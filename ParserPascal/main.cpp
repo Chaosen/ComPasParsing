3ef9753ac36d995334d19fda04b29b193b938228
DaniyarZh <den-gak@mail.ru>
2015-10-19 18:50:14 +0400

    Добавлен интерфейс пользователя

--- ParserPascal/main.cpp
+++ ParserPascal/main.cpp
@@ -10,10 +10,25 @@ using namespace std;
 
 int main() {
 	string stroka;
-	cin >> stroka;
+	setlocale(LC_ALL, "Russian");
 	Parcer p("Syntax.txt");
-	cout<<p.isKeyword(stroka);
-
+	
+	bool exit = 0;
+	while (exit)
+	{
+		cout << "Введите ключевое слово" << endl;
+		cin >> stroka;
+		if (p.isKeyword(stroka))
+			cout << "Является ключевым словом" << endl;
+		else
+			cout << "Не является ключевым словом" << endl;
+		cout << "Чтобы проверить следующее слово нажмите - 1 , Выйти - 0" << endl;
+		bool ok = 1;
+		cin >> ok;
+		if (!ok)
+			exit = 0;
+	}
+	
 	system("pause");                             
 	return 0;                                   
 }
